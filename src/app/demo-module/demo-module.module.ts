c06e1c8aa9c1225bc9b81c8fe638615d9e624f8d
Zeyad Mohsen <127048807+zeyadMohsenHegazy@users.noreply.github.com>
2023-09-14 09:52:39 +0300

    Sturcture Cearation

---
+++ src/app/demo-module/demo-module.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DemoPageComponent } from './demo-page/demo-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    DemoPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DemoModuleModule { }
